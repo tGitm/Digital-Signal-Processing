9b1dd658c68d17277df815aae30bbf93e3a1193e
tGitm <timrus99@gmail.com>
2021-01-12 14:43:19 +0100

    Seminar3 report update

--- Seminar3/seminar3.m
+++ Seminar3/seminar3.m
@@ -12,42 +12,47 @@ Fs = 22050;
 
 
 %task2
-out = filter(filterF(420, 430, 450, 460, .5,  60, 1, Fs, 'stopband'),read); %applies band stop filter to the audio
+%uporabim stopband filter na zvoku
+out = filter(filterF(420, 430, 450, 460, .5,  60, 1, Fs, 'stopband'),read); 
 
-out2 = filter(filterF(1300, 1310, 1330, 1340, .5,  60, 1, Fs, 'stopband'),out); %applies band stop filter to the audio
+out2 = filter(filterF(1300, 1310, 1330, 1340, .5,  60, 1, Fs, 'stopband'),out);
 
-out3 = filter(filterF(200, 210, 230, 240, .5,  60, 1, Fs, 'stopband'),out2); %applies band stop filter to the audio
+out3 = filter(filterF(2000, 2100, 2300, 2400, .5,  60, 1, Fs, 'stopband'),out2);
 
-out4 = filter(filterF(3060, 3070, 3090, 3100, .5,  60, 1, Fs, 'stopband'),out3); %applies band stop filter to the audio
+out4 = filter(filterF(3060, 3070, 3090, 3100, .5,  60, 1, Fs, 'stopband'),out3); 
 
-out5 = filter(filterF(3940, 3950, 3970, 3980, .5,  60, 1, Fs, 'stopband'),out4); %applies band stop filter to the audio
+out5 = filter(filterF(3940, 3950, 3970, 3980, .5,  60, 1, Fs, 'stopband'),out4); 
 
-out6 = filter(filterF(4820, 4830, 4850, 4860, .5,  60, 1, Fs, 'stopband'),out5); %applies band stop filter to the audio
+out6 = filter(filterF(4820, 4830, 4850, 4860, .5,  60, 1, Fs, 'stopband'),out5); 
 
-sound(out6,Fs); %plays filtered audio file
-audiowrite('izhod.wav',out6,Fs); %writes the newly filtered audio file as a new file
+%zaigra filtriran zvok
+sound(out6,Fs); 
+%zapiše novo filtrirano datoteko
+audiowrite('izhod.wav',out6,Fs);
 
 
 
-%applies the fourier transform to the audio
-firstTransform = fft(read);  %fft to transform the original signal into frequency domain 
-% dft1 has even length 
-firstTransform = firstTransform(1:N/2+1); %we need half of that singal because we dont nek
-f0 = 0:fs/N:fs/2; % create a frequency vector for plot
+%fft za pretvorbo originalnega zvoka
+firstTransform = fft(read); 
+firstTransform = firstTransform(1:N/2+1); 
+%naredim vector frekvence za izris podatkov
+f0 = 0:fs/N:fs/2;
 figure; hold on;
 plot(f0,abs(firstTransform)); 
 
 [read1,fs1] = audioread('izhod.wav');
 N1 = length(read1);
-secondTransform = fft(read1);  %fft to transform the original signal into frequency domain 
-secondTransform = secondTransform(1:N1/2+1); %we need half of that singal because we dont nek
-f1 = 0:fs1/N1:fs1/2; % create a frequency vector for plot
+%fft za pretvorbo originalnega zvoka
+secondTransform = fft(read1);  
+secondTransform = secondTransform(1:N1/2+1);
+%naredim vector frekvence za izris podatkov
+f1 = 0:fs1/N1:fs1/2;
 plot(f1,abs(secondTransform)); 
-title('Amplitude Spectrum of the audio signal after aplying Band Stop Filter'); 
+title('Amplitude Spectrum after aplying Band Stop Filter'); 
 xlabel ('Frequency[Hz]'); ylabel('Amplitude'); grid on; 
 hold off;
 
 function Hd = filterF(Fpass1, Fstop1, Fstop2, Fpass2, Apass1, Astop, Apass2, Fs, match)
     h  = fdesign.bandstop(Fpass1, Fstop1, Fstop2, Fpass2, Apass1, Astop, Apass2, Fs);
     Hd = design(h, 'butter', 'MatchExactly', match); 
-end 
+end
