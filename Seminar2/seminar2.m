70c01d8162a9e1db725f900cc0cba6690343c2c7
tGitm <timrus99@gmail.com>
2021-01-14 17:18:28 +0100

    Seminar3 completed

--- Seminar2/seminar2.m
+++ Seminar2/seminar2.m
@@ -4,10 +4,10 @@
 [y4,Fs4] = audioread('born_yesterday.wav');
 %sound(y1, Fs1);
 
-stft(y1,Fs1,'Window',kaiser(256,5),'OverlapLength',220,'FFTLength',512);
+%stft(y1,Fs1,'Window',kaiser(256,5),'OverlapLength',220,'FFTLength',512);
 %stft(y2,Fs2,'Window',kaiser(256,5),'OverlapLength',220,'FFTLength',512);
 %stft(y3,Fs3,'Window',kaiser(256,5),'OverlapLength',220,'FFTLength',512);
-%stft(y4,Fs4,'Window',kaiser(256,5),'OverlapLength',220,'FFTLength',512);
+stft(y4,Fs4,'Window',kaiser(256,5),'OverlapLength',220,'FFTLength',512);
 
 
 
